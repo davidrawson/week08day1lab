cd08885c1c70e193358a4cc2beac9feef5fe0417
David Rawson <dm.rawson@gmail.com>
2018-01-22 19:01:52 +0000

    Initial commit

---
+++ app/src/main/java/example/codeclan/com/wordcounter/WordCounterActivity.java
@@ -0,0 +1,34 @@
+package example.codeclan.com.wordcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class WordCounterActivity extends AppCompatActivity {
+
+    EditText textBlock;
+    Button countButton;
+    TextView wordCountView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_counter);
+
+        textBlock = findViewById(R.id.editText);
+        countButton = findViewById(R.id.button);
+        wordCountView = findViewById(R.id.textView);
+
+    }
+
+    public void onCountButtonClicked(View button){
+        String text = textBlock.getText().toString();
+
+        Log.d(getClass().toString(), "The text entered was " + textBlock);
+
+    }
+}
